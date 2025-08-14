23c9bc1109c926fb45041a86199a9ad14ae5243f
Jacob (jacw) <jacw@odoo.com>
2025-08-14 16:18:32 -0400

    [FIX] techwk3: Added additional comment
    Just added a comment to the code I am overwriting.

--- techwk3/models/pos_session.py
+++ techwk3/models/pos_session.py
@@ -1,6 +1,6 @@
 from odoo import fields, models
 
-class PosSession(model.Models):
+class PosSession(models.Model):
     _inherit = 'pos.session'
 
     # original code: https://github.com/odoo/odoo/blob/a456d9c7cbdf17edb5db2c73306b62150e46a7a7/addons/point_of_sale/models/pos_session.py#L2070
