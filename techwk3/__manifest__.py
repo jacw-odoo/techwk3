010326baedbe33b3172e930df5e9fd276c4b11f3
Jacob (jacw) <jacw@odoo.com>
2025-08-15 11:08:27 -0400

    [IMP] techwk3: Change bold document template with inherited views.
    Inheriting views is safer (and more efficient) than re-creating the
    entire view. An even simpler version of this might be to call the
    specific header element from the box, but I do not think this is
    possible.
    
    opw-4986417

--- techwk3/__manifest__.py
+++ techwk3/__manifest__.py
@@ -2,9 +2,10 @@
     "name": "Tech Week 3",
     "version": "17.0.0.0.0",
     "license": "OEEL-1",
-    "depends": ["base"],
+    "depends": ["base","web"],
     "data":[
         # SECURITY
         # VIEWS
+        "views/report_templates.xml"
     ],
 }
