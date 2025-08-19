f4718387b7a66b8108b38da298869bf94ff51f71
Jacob (jacw) <jacw@odoo.com>
2025-08-19 09:02:22 -0400

    [IMP] techwk3: Minor tweaks, re-added product count deincrement.
    Storing the ID for the user's contact's commercial partner will
    save time and space compared to the previous code.
    
    Also, I was not able to replicate the previous issues with the
    product count, so I added the code back.
    
    opw-4986419

--- techwk3/controllers/main.py
+++ techwk3/controllers/main.py
@@ -7,22 +7,20 @@ class NewWebsiteSale(WebsiteSale):
 
     def _shop_lookup_products(self, attrib_set, options, post, search, website):
         fuzzy_search_term, product_count, search_result = super()._shop_lookup_products(attrib_set, options, post, search, website)
-        # need to connect the UID to a partner record first
-        user = search_result.env.user
-        # iterate over list, removing products that shan't be seen
+        user_commercial_id = search_result.env.user.partner_id.commercial_partner_id.id
+
         for product in search_result:
             product.ensure_one()
             # fails if no partner set, or if user/their parent do not match product's user/parent.
-            # note: DIDO mentioned that the logic I used could be replaced with the "&" operator between 2 recordsets.
-            #       also that the field commercial_partner_id is the useful field when you want company or the user.
-            if product.partner_allowed_id.id == False or user.partner_id.commercial_partner_id.id != product.partner_allowed_id.commercial_partner_id.id:
+            # note: I had worse logic before (comparing partners and their parents). Dirk reminded
+            #       me that the field "commercial_partner_id" is useful here, either parent or self
+            if product.partner_allowed_id.id == False or user_commercial_id != product.sudo().partner_allowed_id.commercial_partner_id.id:
                 search_result = search_result - product
-                # product_count -= 1
+                product_count -= 1
         
-        #return None
         return fuzzy_search_term, product_count, search_result
     
-    # TODO: simplify this method, look into controller arg auth='user'
+    # TODO: simplify this method, look into controller arg auth='user', add product
     @http.route([
         '/shop',
         '/shop/page/<int:page>',
@@ -30,9 +28,6 @@ class NewWebsiteSale(WebsiteSale):
         '/shop/category/<model("product.public.category"):category>/page/<int:page>',
     ], type='http', auth="public", website=True, sitemap=WebsiteSale.sitemap_shop)
     def shop(self):
-        print('debug')
-        if request.env.user._is_public(): #request.session._is_public_user():
+        if request.env.user._is_public():
             return request.redirect("/web/login")
         return super().shop(self)
-
-    # TODO: add product restriction code
