7861780409101dfea87783d9e144c54a1b5c6d11
Jacob (jacw) <jacw@odoo.com>
2025-08-19 11:36:22 -0400

    [IMP] techwk3: grab previous sale orders.
    Committing early and often! While I am not sure how this feature
    should work with multiple previous sale orders (pick latest ID,
    pick latest date, etc), this is a start.
    
    opw-4986424

--- techwk3/__manifest__.py
+++ techwk3/__manifest__.py
@@ -2,7 +2,7 @@
     "name": "Tech Week 3",
     "version": "17.0.0.0.0",
     "license": "OEEL-1",
-    "depends": ["sale"],
+    "depends": ["sale_management"],
     "data":[
         # SECURITY
         # VIEWS
--- techwk3/models/sale_order_line.py
+++ techwk3/models/sale_order_line.py
@@ -1,4 +1,20 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
+
+    price_difference = fields.Boolean(compute="_compute_price_difference",store=True)
+
+    @api.depends("price_unit")
+    def _compute_price_difference(self):
+        # assume no difference
+        self.price_difference = False
+        # get list of orders
+        prev_orders_with_prod = self.order_partner_id.sale_order_ids.filtered_domain([
+            '&', '&',
+                ('id','!=',self.order_id.id),
+                ('order_line.product_id','=',self.product_id.id),
+                ('state','=','sale')
+            ])
+        # look through each order
+        return True
