df18f4f93c15b24cfd489af809db21930cfe70bc
Jacob (jacw) <jacw@odoo.com>
2025-08-20 17:43:42 -0400

    [IMP] techwk3: Ground work for ir.sequence
    I did not get it to work yet, but I am developing a way to
     a) Automatically create missing sequence records, if they do not
        exist already
     b) Use the sequence record to generate a new string.
    
    I'll work on it more tomorrow.
    
    opw-4986423

--- techwk3/models/product_product.py
+++ techwk3/models/product_product.py
@@ -3,13 +3,15 @@ from odoo import api,fields,models
 class ProductProduct(models.Model):
     _inherit = "product.product"
 
-    product_group = fields.Selection([('pr','Printer'),('re','Reader'),('sc','Scanner')])
+    product_group = fields.Selection([('print','Printer'),('read','Reader'),('scan','Scanner')])
     barcode = fields.Char(compute="_compute_barcode", store=True, readonly=False)
 
     @api.depends("product_group")
     def _compute_barcode(self):
-        if self.product_group and self.barcode == False or self.product_group.lower() != self.barcode[:2]:
-            self.barcode = self.product_group.upper() + '.' + '{:0>6}'.format((type(self.id) == int and self.id) or self.id.origin)
-        else:
-            self.barcode = False
-        
+        for product in self:
+            if not product.barcode or (product.product_group and
+                    product.product_group[:2].upper() != product.barcode[:2]):
+                product._create_product_group_sequence(product.pro)
+                product.barcode = product.env["ir.sequence"].next_by_code('product.product.{group}')
+            else:
+                product.barcode = False
